31055c0689046ad5493558632a940171d65adc39
Fedor Protkov <fedorprotkov@Fedors-MacBook-Pro.local>
2018-01-27 11:32:16 -0500

    Stigma pulses and gets triggered when hb collides with first collider

--- 8102jgg/Assets/Scripts/colliders_RegFlower.cs
+++ 8102jgg/Assets/Scripts/colliders_RegFlower.cs
@@ -4,27 +4,45 @@ using UnityEngine;
 
 //source : https://answers.unity.com/questions/257569/tag-if-condition-with-ontriggerenter.html
 
-public class colliders_RegFlower : MonoBehaviour {
+public class colliders_RegFlower : MonoBehaviour
+{
 
+	public bool findNectar = false;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 //		Debug.Log("hit outter collider");	
 	}
 
-	void OnTriggerEnter(Collider theCollision){
+	void OnTriggerEnter (Collider theCollision)
+	{
 
-		if (theCollision.gameObject.tag == "OutterLayer_RegFlower"){
-		Debug.Log("hit outter collider");
+		if (theCollision.gameObject.tag == "OutterLayer_RegFlower")
+		{
+			Debug.Log ("hit outter collider");
+			findNectar = true;
 		}
 
-		if(theCollision.gameObject.tag == "stigma"){
-			Debug.Log("hit the stigma");
+
+		if (theCollision.gameObject.tag == "stigma")
+		{
+			Debug.Log ("hit the stigma");
+		}
+	}
+
+	void OnTriggerExit (Collider theCollision)
+	{
+		if (theCollision.gameObject.tag == "OutterLayer_RegFlower")
+		{
+//			Debug.Log ("hit outter collider");
+			findNectar = false;
 		}
 	}
 }
