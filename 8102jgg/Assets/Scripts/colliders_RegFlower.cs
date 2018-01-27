4752acba850acb2f3546a4268a44893a6b151b2c
Isabelle Charette <isabelle.b.charette@gmail.com>
2018-01-27 02:25:13 -0500

    made prefab for regular flower and HB. HB has 2 scripts attached : one for moving according arrows on keybord and the other for detecting collisions with either the outter collider of the flower or the stigma (collissions printed on console)

---
+++ 8102jgg/Assets/Scripts/colliders_RegFlower.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//source : https://answers.unity.com/questions/257569/tag-if-condition-with-ontriggerenter.html
+
+public class colliders_RegFlower : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Debug.Log("hit outter collider");	
+	}
+
+	void OnTriggerEnter(Collider theCollision){
+
+		if (theCollision.gameObject.tag == "OutterLayer_RegFlower"){
+		Debug.Log("hit outter collider");
+		}
+
+		if(theCollision.gameObject.tag == "stigma"){
+			Debug.Log("hit the stigma");
+		}
+	}
+}
