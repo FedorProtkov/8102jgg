a1e125a69cf73218c310ef089058cbeda9d210c6
TheyCallMeJQ <jaquesmyster@hotmail.com>
2018-01-28 08:29:12 -0500

    Update

--- 8102jgg/Assets/Scripts/Managers/BeakColorManager.cs
+++ 8102jgg/Assets/Scripts/Managers/BeakColorManager.cs
@@ -8,21 +8,27 @@ public class BeakColorManager : MonoBehaviour {
 
 	private Renderer m_renderer;
 
-	public float floor = 0.3f;
-	public float ceiling = 1.0f;
-	public float oscSpeed = 1.0f;
+	public float floor;
+	public float ceiling;
+	public float oscSpeed;
 	private Color beakColor;
+	/**An array of all our colors, to facilitate switching to the next color in the sequence.*/
+	private Color[] all_colors = { Color.blue, Color.red, Color.blue, Color.white, 
+								Color.magenta, Color.yellow, Color.cyan, Color.red};
+	/**An int representative of the index of the current color*/
+	private int m_ColorIndex = 0;
+
+
 	// Use this for initialization
 	void Start ()
 	{
-		this.beakColor = Color.cyan;
+		this.beakColor = all_colors[this.m_ColorIndex];
 		this.m_renderer = this.m_Bird.GetComponentInChildren<Renderer> ();
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-//		Renderer renderer = GetComponent<Renderer> ();
 		Material mat = this.m_renderer.material;
 		float emission = floor + Mathf.PingPong (Time.time * oscSpeed, ceiling - floor);
 		Color baseColor = beakColor; //Replace this with whatever you want for your base color at emission level '1'
@@ -32,8 +38,22 @@ public class BeakColorManager : MonoBehaviour {
 		mat.SetColor ("_EmissionColor", finalColor);
 	}
 
+	/**A function to change the beak color to a given color.*/
 	public void ChangeBeakColor(Color color)
 	{
 		this.beakColor = color;
+	}
+
+	/**A color to change the beak color to the next color in the sequence.*/
+	public void ChangeBeakColorToNextInSequence()
+	{
+		this.m_ColorIndex++;
+		this.beakColor = this.all_colors [this.m_ColorIndex];
+	}
+
+	/**A function to return the current color of the beak; this is meant to help us with the guiding particle effects.*/
+	public Color GetCurrentBeakColor()
+	{
+		return this.beakColor;
 	}
 }
---
+++ 8102jgg/Assets/Scripts/Managers/GuidingFireflyManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuidingFireflyManager : MonoBehaviour {
+	/**A reference to the firefly that guides the player to the next flower to be pollenated*/
+	[SerializeField] GameObject m_GuidingFirefly;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void ManageMovement()
+	{
+
+	}
+
+	private void CircleFlower()
+	{
+
+	}
+}
